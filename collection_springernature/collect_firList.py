855297b7303ef23e072f66cb480768181f1b7a43
Yanxi1024 <143350722+Yanxi1024@users.noreply.github.com>
2024-12-12 18:46:25 -0500

    Update collect_firList.py

--- collection_springernature/collect_firList.py
+++ collection_springernature/collect_firList.py
@@ -10,7 +10,7 @@ import time
 page_num = 6
 
 #related config
-srcdb = 'springernature'
+srcdb = 'This is srcdb'
 url = 'https://experiments.springernature.com/'
 uri = 'mongodb://This is MongoDB'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
