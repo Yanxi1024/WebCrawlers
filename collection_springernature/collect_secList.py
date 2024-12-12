66935cc25473e675c4f303e7ca55f49f12f83a5f
Yanxi1024 <143350722+Yanxi1024@users.noreply.github.com>
2024-12-12 18:46:09 -0500

    Update collect_secList.py

--- collection_springernature/collect_secList.py
+++ collection_springernature/collect_secList.py
@@ -9,7 +9,7 @@ import time
 
 #related config
 GlobalCollectorNum = 30
-srcdb = 'springernature'
+srcdb = 'This is srcdb'
 uri = 'mongodb://This is MongoDB'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
 proxies = {
